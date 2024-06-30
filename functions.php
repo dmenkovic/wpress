d836954c1014793b01139fd077c9c95b114de59b
Dobrica Menković <menkovicd.83@gmail.com>
2024-06-30 11:34:27 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -16,6 +16,8 @@ Pozivanje funkcije na mestu u kodu gde želimo vidljivost tj. da postavimo vidž
 Preko funcije u ovom fajlu pravimo mesto u Wpressu za vidžet. Na tom mestu u wp možemo ubaciti potrebne vidžet
 e i pozvati funciju u kodu gde želimo da se prikaže: heder, futer...
 
-Plugin: Easy Social Icons - instal i aktiv ovaj plagin da bismo mogli u widget da ubacimo socijalne mreže
+Widget Plugins: 
+* Easy Social Icons - instal i aktiv ovaj plagin da bismo mogli u widget da ubacimo socijalne mreže
 all icons već ima neke predefinisane, možemo ih obrisati jer imamo mogućnost da sami unesemo naš dizajn ikonica
 Tu ćemo dodeliti i id ili klasu preko kojih ćemo ih stilizovati
+* Black Studio TinyMCE Widget - za unos teksta sa više opcija ima opciju Add Media
